3ec495c896d4f35c40bdd668d9fe457c99964ea2
Alper <alperonline@hotmail.com>
2017-02-08 17:29:51 +0300

    Added files

---
+++ RedisClient/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace RedisClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new SimpleRedisClient("localhost", 6379);
+            client.DoJob();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedisClient/SimpleRedisClient.cs
@@ -0,0 +1,54 @@
+﻿using CacheManager.Core;
+
+namespace RedisClient
+{
+    public class SimpleRedisClient
+    {
+        private readonly ICacheManager<ComplexCacheItem> _cache;
+
+        public SimpleRedisClient(string host, int port)
+        {
+            _cache = CacheFactory.Build<ComplexCacheItem>(settings =>
+            {
+                settings
+                    .WithRedisConfiguration("redis", config =>
+                    {
+                        config.WithAllowAdmin()
+                            .WithDatabase(0)
+                            .WithEndpoint(host, port);
+                    })
+                    .WithMaxRetries(1000)
+                    .WithRetryTimeout(100)
+                    .WithRedisBackplane("redis")
+                    .WithRedisCacheHandle("redis", true);
+            });
+        }
+
+        public void DoJob()
+        {
+            //Add item to cache
+            var cacheItem = new ComplexCacheItem();
+            _cache.Add("myKey", cacheItem);
+
+
+            //Get item from cache
+            ComplexCacheItem itemFromCache = _cache.Get("myKey");
+            itemFromCache.ChildObject.CacheVisitCount++;
+
+            //Update item in cache.
+            _cache.Update("myKey", item => itemFromCache);
+
+            //Check if modification is done
+            itemFromCache = _cache.Get("myKey");
+
+
+            //Remove item from cache.
+            _cache.Remove("myKey");
+
+            //Check if item removed
+            itemFromCache = _cache.Get("myKey");
+
+        }
+
+    }
+}
